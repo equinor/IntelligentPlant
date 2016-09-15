06ca61255ecc61593d78858ac95750c6289962da
Ivan Metrikin <ivan.metrikin@gmail.com>
2016-09-15 14:24:57 +0200

    Fixed AWS lambda function

--- ProcessDynamoDBStream.py
+++ ProcessDynamoDBStream.py
@@ -5,8 +5,12 @@ from datetime import datetime
 def lambda_handler(event, context):
     cwc=boto3.client('cloudwatch', region_name='us-west-2')
     for record in event['Records']:
+        payload = record['dynamodb']['NewImage']['payload']
+        print(payload)
         sensor_name = record['dynamodb']['NewImage']['payload']['M']['name']['S']
-        event_value = record['dynamodb']['NewImage']['payload']['M']['value']['N']
+        try: event_value = record['dynamodb']['NewImage']['payload']['M']['value']['N']
+        except Exception as e:            
+            event_value=0
         event_value = float(event_value)
         event_datetime = record['dynamodb']['NewImage']['payload']['M']['timestamp']['S']
         #print(sensor_name)
